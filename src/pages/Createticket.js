cbdbd8028a064ca0f00090d816c3a6bb35604e4a
oscar120020 <omartinez@intellisys.com.do>
2022-01-11 16:39:44 -0400

    componente de cola

--- src/pages/Createticket.js
+++ src/pages/Createticket.js
@@ -1,9 +1,42 @@
+import { DownCircleOutlined } from '@ant-design/icons'
+import { Button, Col, Divider, Row, Typography } from 'antd'
 import React from 'react'
 
+const { Title, Text } = Typography
+
 export const Createticket = () => {
+
+    const newTicket = () => {
+        console.log("new ticket");
+    }
+
     return (
-        <div>
-            crear ticket
-        </div>
+        <>
+            <Row>
+                <Col span={14} offset={6} align="center">
+                    <Title>Presione el botón para un nuevo ticket</Title>
+                    <Button
+                        type="primary"
+                        shape="round"
+                        icon={<DownCircleOutlined/>}
+                        size="large"
+                        onClick={newTicket}
+                    >
+                        Nuevo ticket
+                    </Button>
+                </Col>
+            </Row>
+            <Row style={{marginTop: 100}}>
+                <Col span={14} offset={6} align="center">
+                    <Text level={2} >
+                        Su número
+                    </Text>
+                    <br/>
+                    <Text type="success" style={{fontSize: 50}} >
+                        10
+                    </Text>
+                </Col>
+            </Row>
+        </>
     )
 }
